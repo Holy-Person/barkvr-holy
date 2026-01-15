4d27d9a851b023227a0ea38b9f83ed53fe0ebd17
Holy-Person <holyochi@gmail.com>
2026-01-15 19:36:46 +0100

    Add Dedicated Property List
    Finally got an actual rewrite of the list done.
    Capable of adding nullable types and categories.
    Deletes the entire BoxContainer to speed up clearing of the previous list.
    Basic group hints are also here.

---
+++ barkvr-system/ui/3dPanel/editmode/inspector/components/property-elements/misc_property_list.gd
@@ -0,0 +1,134 @@
+class_name InspectorPropertyList
+extends Container
+## A control used to edit the properties of an object.
+##
+## This is the control that holds and generates the list to edit properties with.
+## Based on Godot's EditorInspector.
+
+
+
+# TODO: find a more optimized way to update field values
+# This requires a PR to godot. they don't seem interested in the change
+# but without it, it's impossible to do performant scene tracking - Zodie
+
+
+
+const PROPERTY_CATEGORY = preload("uid://cw7vdx8shf5ca")
+
+const FIELD_BOOL = preload("uid://bcgft7j8haksh")
+const FIELD_INT = preload("uid://dyiaij1fj5sje")
+const FIELD_FLOAT = preload("uid://po0psf0jy7wg")
+const FIELD_STRING = preload("uid://bpcdhejgbrn6i")
+const FIELD_COLOR = preload("uid://1ynmsuc1l8yy")
+const FIELD_VECTOR_2 = preload("uid://dsinfbuvvxpxu")
+const FIELD_VECTOR_3 = preload("uid://d0negxx0bii55")
+
+## Button group to ensure only one field is active at a time.
+var list_button_group := ButtonGroup.new()
+
+## The current object being edited.
+var current_object: Object
+## VBoxContainer to hold the properties.
+var v_box_container: VBoxContainer
+
+
+
+func edit(object: Object) -> void:
+	if v_box_container: v_box_container.queue_free()
+	current_object = object
+
+	if not is_instance_valid(object): return
+
+	print("Setting target in: ",self, " to look at: ", object)
+
+	v_box_container = VBoxContainer.new()
+	v_box_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	v_box_container.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	add_child(v_box_container)
+
+	var start_time: float = Time.get_ticks_msec()
+
+	for property: Dictionary in object.get_property_list():
+		# Discard everything if selection has changed.
+		if object != current_object: return
+
+		print(property)
+
+		# Delay for performance.
+		if start_time + 1 < Time.get_ticks_msec():
+			await get_tree().process_frame
+			start_time = Time.get_ticks_msec()
+
+		# Unknown discard.
+		if property.name == "owner":
+			continue
+
+		# Category headers.
+		if (property.usage & PROPERTY_USAGE_CATEGORY) > 0:
+			_add_category(property)
+		# Groups. (Going to be really annoying to work with.)
+		elif (property.usage & PROPERTY_USAGE_GROUP) > 0:
+			_add_group(property)
+		else:
+			if not is_instance_valid(object): return
+			_add_property(object, property)
+
+
+
+## Returns the object currently selected in this inspector.
+func get_edited_object() -> Object:
+	return current_object
+
+
+
+func _add_category(property: Dictionary) -> void:
+	var category = PROPERTY_CATEGORY.instantiate()
+	v_box_container.add_child(category)
+	category.set_data(property)
+
+func _add_group(property: Dictionary) -> void:
+	var label := Label.new()
+	label.text = property.name
+	v_box_container.add_child(label)
+
+func _add_property(object: Object, property: Dictionary) -> void:
+	var property_field: PropertyBase
+
+	match property.type:
+		TYPE_BOOL:
+			property_field = FIELD_BOOL.instantiate()
+		TYPE_INT:
+			if property.hint == 2:
+				pass
+				#property_field = FIELD_ENUM.instantiate()
+				#tmp.set_data(fieldname, new_target, prop.name, prop)
+			else:
+				property_field = FIELD_INT.instantiate()
+		TYPE_FLOAT:
+			property_field = FIELD_FLOAT.instantiate()
+		TYPE_STRING:
+			property_field = FIELD_STRING.instantiate()
+		TYPE_STRING_NAME:
+			if property.hint == 2:
+				pass
+				#property_field = FIELD_ENUM.instantiate()
+				#tmp.set_data(fieldname, new_target, prop.name, prop, true)
+			else:
+				property_field = FIELD_STRING.instantiate()
+		TYPE_COLOR:
+			property_field = FIELD_COLOR.instantiate()
+		TYPE_VECTOR2, TYPE_VECTOR2I:
+			property_field = FIELD_VECTOR_2.instantiate()
+		TYPE_VECTOR3, TYPE_VECTOR3I:
+			property_field = FIELD_VECTOR_3.instantiate()
+		TYPE_ARRAY:
+			pass
+		TYPE_OBJECT:
+			pass
+		_:
+			print("Non-handled type: ", property.type)
+
+	if property_field:
+		v_box_container.add_child(property_field)
+		property_field.button_group = list_button_group
+		property_field.set_data(object, property)
--- barkvr-system/ui/3dPanel/editmode/inspector/panels/panel_properties.gd
+++ barkvr-system/ui/3dPanel/editmode/inspector/panels/panel_properties.gd
@@ -1,23 +1,6 @@
 extends InspectorPanel
 
-# TODO: find a more optimized way to update field values
-# This requires a PR to godot. they don't seem interested in the change
-# but without it, it's impossible to do performant scene tracking
 
-#const FIELD_ARRAY = preload("uid://dxshup2m2t62c")
-#const FIELD_BOOL = preload("uid://bcgft7j8haksh")
-#const FIELD_COLOR = preload("uid://1ynmsuc1l8yy")
-#const FIELD_ENUM = preload("uid://dy1wide8xv5df")
-#const FIELD_INT = preload("uid://d1t7rsyo8nq76")
-#const FIELD_NUMBER = preload("uid://cbmgba1q1d8q5")
-#const FIELD_OBJECT = preload("uid://dddtbbin6ty1l")
-#const FIELD_STRING = preload("uid://3cx3afcwx3hw")
-
-const FIELD_VECTOR_2 = preload("uid://dsinfbuvvxpxu")
-const FIELD_VECTOR_3 = preload("uid://d0negxx0bii55")
-
-# Type ignores naming conventions.
-var event_manager : Bark_Journal
 
 @onready var target_name_line: LineEdit = %TargetName
 
@@ -26,113 +9,25 @@ var event_manager : Bark_Journal
 @onready var button_next: Button = %ButtonNext
 @onready var button_history: Button = %ButtonHistory
 
-@onready var scroll_list: VBoxContainer = %ScrollList
+@onready var property_list: InspectorPropertyList = %PropertyList
 
-# Leftover here.
-#@onready var activetoggle: CheckButton = $"VBoxContainer/titlebar/properties header/active/HBoxContainer/activetoggle"
 
-#var target : Object = null
 
 func _ready() -> void:
-	event_manager = Engine.get_singleton(&"event_manager")
-
-	_setup_signals()
-
-func _setup_signals() -> void:
-	#activetoggle.toggled.connect(func(on: bool):
-	#	if target and is_instance_valid(target) and target is Node:
-	#		event_manager.set_property(event_manager.root.get_path_to(target), "visible", on)
-	#	)
 	target_name_line.text_changed.connect(_on_target_name_line_text_changed)
 
 func _on_target_set(new_target: Node) -> void:
-	print("setting target in: ",self, "to look at: ",new_target)
+	if not is_instance_valid(new_target): return
 
-	if new_target and new_target is Object:
-		target = new_target
-		if "name" in new_target and new_target.name:
-			target_name_line.text = new_target.name
-		if new_target.has_meta("display_name"):
-			target_name_line.text = new_target.get_meta("display_name")
+	# Set text in TargetName LineEdit.
+	if new_target.has_meta(&"display_name"):
+		target_name_line.text = new_target.get_meta(&"display_name")
+	elif "name" in new_target and new_target.name:
+		target_name_line.text = new_target.name
 
-		#activetoggle.button_pressed = new_target.visible
+	property_list.edit.call_deferred(new_target)
 
-		var start_time : float = Time.get_ticks_msec()
-		for child in scroll_list.get_children():
-			if start_time + 1 < Time.get_ticks_msec():
-				await get_tree().process_frame
-				start_time = Time.get_ticks_msec()
-			child.queue_free()
-		var prop_list :Array[Dictionary]= new_target.get_property_list()
-		call_deferred("_add_fields", prop_list, new_target)
 
-func _add_fields(property_list, new_target) -> void:
-	while LocalGlobals.is_inspector_loading:
-		await get_tree().process_frame
-	LocalGlobals.is_inspector_loading = true
-
-	var button_group := ButtonGroup.new()
-
-	var start_time : float = Time.get_ticks_msec()
-
-	for property in property_list:
-		if start_time + 1 < Time.get_ticks_msec():
-			await get_tree().process_frame
-			start_time = Time.get_ticks_msec()
-
-		if !is_instance_valid(new_target):
-			return # Can cause inspector loading to get stuck at true
-
-		if property.name == "owner":
-			continue
-
-		var property_field: PropertyBase
-		match property.type:
-			#TYPE_OBJECT:
-			#	property_field = FIELD_OBJECT.instantiate()
-			#TYPE_ARRAY:
-			#	property_field = FIELD_ARRAY.instantiate()
-			#TYPE_STRING_NAME:
-			#	match property.hint:
-			#		0:
-			#			property_field = FIELD_STRING.instantiate()
-			#		2:
-			#			property_field = FIELD_ENUM.instantiate()
-			#			tmp.set_data(fieldname, new_target, prop.name, prop, true)
-			#TYPE_STRING:
-			#	property_field = FIELD_STRING.instantiate()
-			#TYPE_COLOR:
-			#	property_field = FIELD_COLOR.instantiate()
-			#TYPE_BOOL:
-			#	property_field = FIELD_BOOL.instantiate()
-			#TYPE_FLOAT:
-			#	property_field = FIELD_NUMBER.instantiate()
-			#	#tmp.type = 0
-			#TYPE_INT:
-			#	match property.hint:
-			#		2:
-			#			property_field = FIELD_ENUM.instantiate()
-			#			#tmp.set_data(fieldname, new_target, prop.name, prop)
-			#		_:
-			#			property_field = FIELD_INT.instantiate()
-			TYPE_VECTOR3, TYPE_VECTOR3I:
-				property_field = FIELD_VECTOR_3.instantiate()
-			TYPE_VECTOR2, TYPE_VECTOR2I:
-				property_field = FIELD_VECTOR_2.instantiate()
-
-		if property_field:
-			scroll_list.add_child(property_field)
-			property_field.button_group = button_group
-			property_field.set_data(target, property)
-
-	LocalGlobals.is_inspector_loading = false
-
-func clear_fields():
-	if target:
-		if target.has_meta(&"display_name"):
-			target_name_line.text = target.get_meta(&"display_name")
-		else:
-			target_name_line.text = target.name
 
 ## Change the name of the currently selected node by typing in the target name field.
 func _on_target_name_line_text_changed(new_text: String) -> void:
