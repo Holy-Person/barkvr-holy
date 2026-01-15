2156ca9004876db848f0e23eb42d1a87e4235c3d
Holy-Person <holyochi@gmail.com>
2026-01-15 03:35:59 +0100

    Use New Property Fields
    Minor refactors to the existing code, replacing the script-based icons, using the vector property-fields for testing.

--- barkvr-system/ui/3dPanel/editmode/inspector/panels/panel_properties.gd
+++ barkvr-system/ui/3dPanel/editmode/inspector/panels/panel_properties.gd
@@ -4,15 +4,17 @@ extends InspectorPanel
 # This requires a PR to godot. they don't seem interested in the change
 # but without it, it's impossible to do performant scene tracking
 
-const FIELD_ARRAY = preload("uid://dxshup2m2t62c")
-const FIELD_BOOL = preload("uid://ctvd4j8wp7mh5")
-const FIELD_COLOR = preload("uid://5r83rbiuayop")
-const FIELD_ENUM = preload("uid://dy1wide8xv5df")
-const FIELD_NUMBER = preload("uid://cbmgba1q1d8q5")
-const FIELD_OBJECT = preload("uid://dddtbbin6ty1l")
-const FIELD_STRING = preload("uid://3cx3afcwx3hw")
-const FIELD_VECTOR_2 = preload("uid://dkaactpqyjgjn")
-const FIELD_VECTOR_3 = preload("uid://dd2kp02usski5")
+#const FIELD_ARRAY = preload("uid://dxshup2m2t62c")
+#const FIELD_BOOL = preload("uid://bcgft7j8haksh")
+#const FIELD_COLOR = preload("uid://1ynmsuc1l8yy")
+#const FIELD_ENUM = preload("uid://dy1wide8xv5df")
+#const FIELD_INT = preload("uid://d1t7rsyo8nq76")
+#const FIELD_NUMBER = preload("uid://cbmgba1q1d8q5")
+#const FIELD_OBJECT = preload("uid://dddtbbin6ty1l")
+#const FIELD_STRING = preload("uid://3cx3afcwx3hw")
+
+const FIELD_VECTOR_2 = preload("uid://dsinfbuvvxpxu")
+const FIELD_VECTOR_3 = preload("uid://d0negxx0bii55")
 
 # Type ignores naming conventions.
 var event_manager : Bark_Journal
@@ -34,14 +36,7 @@ var event_manager : Bark_Journal
 func _ready() -> void:
 	event_manager = Engine.get_singleton(&"event_manager")
 
-	_setup_icons()
 	_setup_signals()
-
-func _setup_icons() -> void:
-	button_documentation.set_button_icon(get_editor_icon(&"HelpSearch"))
-	button_previous.set_button_icon(get_editor_icon(&"Back"))
-	button_next.set_button_icon(get_editor_icon(&"Forward"))
-	button_history.set_button_icon(get_editor_icon(&"History"))
 
 func _setup_signals() -> void:
 	#activetoggle.toggled.connect(func(on: bool):
@@ -71,101 +66,65 @@ func _on_target_set(new_target: Node) -> void:
 		var prop_list :Array[Dictionary]= new_target.get_property_list()
 		call_deferred("_add_fields", prop_list, new_target)
 
-func _add_fields(prop_list, new_target) -> void:
+func _add_fields(property_list, new_target) -> void:
 	while LocalGlobals.is_inspector_loading:
 		await get_tree().process_frame
 	LocalGlobals.is_inspector_loading = true
+
+	var button_group := ButtonGroup.new()
+
 	var start_time : float = Time.get_ticks_msec()
-	for i in range(prop_list.size()):
+
+	for property in property_list:
 		if start_time + 1 < Time.get_ticks_msec():
 			await get_tree().process_frame
 			start_time = Time.get_ticks_msec()
-		#await get_tree().process_frame
-		var prop = prop_list[i]
-		var fieldname :String= prop.name
-		if prop.name.contains("bones/") and new_target is Skeleton3D:
-			fieldname = "bone: "+new_target.get_bone_name(int(prop.name.split("/")[1]))+" "+prop.name.split("/")[-1]
+
 		if !is_instance_valid(new_target):
-			return
-		if prop.name == "owner":
+			return # Can cause inspector loading to get stuck at true
+
+		if property.name == "owner":
 			continue
-		match prop.type:
-			TYPE_OBJECT:
-				var tmp :Object_Attribute = FIELD_OBJECT.instantiate()
-				scroll_list.add_child(tmp)
-				tmp.name = fieldname
-				tmp.set_data(fieldname, target, prop.name)
-			TYPE_ARRAY:
-				var tmp :Array_Attribute = FIELD_ARRAY.instantiate()
-				scroll_list.add_child(tmp)
-				tmp.name = fieldname
-				tmp.set_data(fieldname, new_target, prop.name)
-			TYPE_STRING_NAME:
-				match prop.hint:
-					0:
-						var tmp :String_Attribute = FIELD_STRING.instantiate()
-						scroll_list.add_child(tmp)
-						tmp.name = fieldname
-						tmp.set_data(fieldname, new_target, prop.name)
-					2:
-						var tmp :Enum_Attribute = FIELD_ENUM.instantiate()
-						scroll_list.add_child(tmp)
-						tmp.name = fieldname
-						tmp.set_data(fieldname, new_target, prop.name, prop, true)
-			TYPE_STRING:
-				var tmp :String_Attribute = FIELD_STRING.instantiate()
-				scroll_list.add_child(tmp)
-				tmp.name = fieldname
-				tmp.set_data(fieldname, new_target, prop.name)
-			TYPE_COLOR:
-				var tmp :Color_Attribute = FIELD_COLOR.instantiate()
-				scroll_list.add_child(tmp)
-				tmp.name = fieldname
-				tmp.set_data(fieldname, new_target, prop.name)
-			TYPE_BOOL:
-				var tmp :Bool_Attribute = FIELD_BOOL.instantiate()
-				scroll_list.add_child(tmp)
-				tmp.name = fieldname
-				tmp.set_data(fieldname, new_target, prop.name)
-			TYPE_FLOAT:
-				var tmp :Number_Attribute = FIELD_NUMBER.instantiate()
-				tmp.type = 0
-				scroll_list.add_child(tmp)
-				tmp.name = fieldname
-				tmp.set_data(fieldname, new_target, prop.name)
-			TYPE_INT:
-				match prop.hint:
-					2:
-						var tmp :Enum_Attribute = FIELD_ENUM.instantiate()
-						scroll_list.add_child(tmp)
-						tmp.name = fieldname
-						tmp.set_data(fieldname, new_target, prop.name, prop)
-					_:
-						var tmp :Number_Attribute = FIELD_NUMBER.instantiate()
-						tmp.type = 1
-						scroll_list.add_child(tmp)
-						tmp.name = fieldname
-						tmp.set_data(fieldname, new_target, prop.name)
-			TYPE_VECTOR3:
-				var tmp :Vector3_Attribute = FIELD_VECTOR_3.instantiate()
-				scroll_list.add_child(tmp)
-				tmp.name = fieldname
-				tmp.set_data(fieldname, new_target, prop.name)
-			TYPE_VECTOR3I:
-				var tmp :Vector3_Attribute = FIELD_VECTOR_3.instantiate()
-				scroll_list.add_child(tmp)
-				tmp.name = fieldname
-				tmp.set_data(fieldname, new_target, prop.name)
-			TYPE_VECTOR2:
-				var tmp :Vector2_Attribute = FIELD_VECTOR_2.instantiate()
-				scroll_list.add_child(tmp)
-				tmp.name = fieldname
-				tmp.set_data(fieldname, new_target, prop.name)
-			TYPE_VECTOR2I:
-				var tmp :Vector2_Attribute = FIELD_VECTOR_2.instantiate()
-				scroll_list.add_child(tmp)
-				tmp.name = fieldname
-				tmp.set_data(fieldname, new_target, prop.name)
+
+		var property_field: PropertyBase
+		match property.type:
+			#TYPE_OBJECT:
+			#	property_field = FIELD_OBJECT.instantiate()
+			#TYPE_ARRAY:
+			#	property_field = FIELD_ARRAY.instantiate()
+			#TYPE_STRING_NAME:
+			#	match property.hint:
+			#		0:
+			#			property_field = FIELD_STRING.instantiate()
+			#		2:
+			#			property_field = FIELD_ENUM.instantiate()
+			#			tmp.set_data(fieldname, new_target, prop.name, prop, true)
+			#TYPE_STRING:
+			#	property_field = FIELD_STRING.instantiate()
+			#TYPE_COLOR:
+			#	property_field = FIELD_COLOR.instantiate()
+			#TYPE_BOOL:
+			#	property_field = FIELD_BOOL.instantiate()
+			#TYPE_FLOAT:
+			#	property_field = FIELD_NUMBER.instantiate()
+			#	#tmp.type = 0
+			#TYPE_INT:
+			#	match property.hint:
+			#		2:
+			#			property_field = FIELD_ENUM.instantiate()
+			#			#tmp.set_data(fieldname, new_target, prop.name, prop)
+			#		_:
+			#			property_field = FIELD_INT.instantiate()
+			TYPE_VECTOR3, TYPE_VECTOR3I:
+				property_field = FIELD_VECTOR_3.instantiate()
+			TYPE_VECTOR2, TYPE_VECTOR2I:
+				property_field = FIELD_VECTOR_2.instantiate()
+
+		if property_field:
+			scroll_list.add_child(property_field)
+			property_field.button_group = button_group
+			property_field.set_data(target, property)
+
 	LocalGlobals.is_inspector_loading = false
 
 func clear_fields():
