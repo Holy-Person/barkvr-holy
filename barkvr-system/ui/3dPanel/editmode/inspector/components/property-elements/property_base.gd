1142211e30147bda5bcc369ab6c8500495ec8673
Holy-Person <holyochi@gmail.com>
2026-01-15 03:31:47 +0100

    Rework Property Base
    Now has a base scene that other property fields will inherit from. Due to the complexity of the parts that get used in all of them, I decided on this approach.

--- barkvr-system/ui/3dPanel/editmode/inspector/components/property-elements/property_base.gd
+++ barkvr-system/ui/3dPanel/editmode/inspector/components/property-elements/property_base.gd
@@ -1,13 +1,49 @@
 class_name PropertyBase
 extends Container
-## Base class for all inspector properties.
+## Custom class for editing properties that can be added to the PropertyPanel.
 ##
-## Contains common variables and functions that all properties use.
+## Contains all variables that every property uses, as well as override functions for easy management.
 
 
 
-@export var label: Label
+@onready var ui_button_background: Button = %ButtonBackground
+@onready var ui_name_container: MarginContainer = %NameField
+@onready var ui_button_checkable: CheckBox = %ButtonCheckable
+@onready var ui_label: Label = %Label
+@onready var ui_button_reset: TextureButton = %ButtonReset
+@onready var ui_editing_field: PanelContainer = %EditingField
 
+
+
+## A checkbox for nullable variant types.
+## TODO: Disable editable content while null.
+var checkable: bool = false:
+	set(value):
+		checkable = value
+		ui_button_checkable.visible = value
+
+## Set this to false to[br]do thing.
+var draw_label: bool = true:
+	set(value):
+		draw_label = value
+		ui_name_container.visible = value
+
+var label: String = "":
+	set(value):
+		label = value
+		ui_label.text = value.capitalize()
+
+var read_only: bool = false:
+	set(value):
+		read_only = value
+		# TODO: Read only state.
+
+var button_group: ButtonGroup:
+	set(value):
+		button_group = value
+		ui_button_background.button_group = button_group
+
+# Type ignores naming conventions.
 var event_manager: Bark_Journal
 
 var target: Object
@@ -21,11 +57,13 @@ var is_editing: bool:
 
 func _ready() -> void:
 	event_manager = Engine.get_singleton(&"event_manager")
+
 	_setup()
+
 	_check_update()
 
 ## Override function.
-## Called after the node is ready.
+## Called after the PropertyBase is ready.
 func _setup() -> void:
 	pass
 
@@ -35,7 +73,7 @@ func _check_update() -> void:
 	var parent: ScrollContainer = get_parent_control().get_parent_control()
 
 	# Don't update if the value is currently being edited.
-	if parent and not is_editing:
+	if target and parent and not is_editing:
 		var parent_rect: Rect2 = parent.get_global_rect()
 		var rect: Rect2 = get_global_rect()
 
@@ -54,17 +92,26 @@ func _update_visual() -> void:
 
 
 
-## Set the important data of the property, indended to be called before the node is ready.
-func set_data(display_name: String, target_object: Object, new_property_name: String) -> void:
-	if label: label.text = display_name
+## Set the target object and property data of this element, indended to be called before the node is ready.
+func set_data(target_object: Object, property: Dictionary) -> void:
 	target = target_object
-	property_name = new_property_name
+	print(property)
+	property_name = property.name
 
-	_on_data_set()
+	if (property.usage & PROPERTY_USAGE_CHECKABLE) > 0:
+		checkable = true
+
+	label = property_name
+
+	if target is Skeleton3D: # Singled out for autocomplete recognition.
+		if property_name.contains("bones/"):
+			label = "bone: " + target.get_bone_name( int(property_name.split("/")[1]) ) + " " + property_name.split("/")[-1]
+
+	_on_data_set(property)
 
 ## Override function.
 ## Called after all data has been set.
-func _on_data_set() -> void:
+func _on_data_set(_property: Dictionary) -> void:
 	pass
 
 
@@ -73,3 +120,8 @@ func _on_data_set() -> void:
 ## Called on get of is_editing.
 func _get_editing_state() -> bool:
 	return false
+
+
+
+func _on_read_only_toggled() -> void:
+	pass
