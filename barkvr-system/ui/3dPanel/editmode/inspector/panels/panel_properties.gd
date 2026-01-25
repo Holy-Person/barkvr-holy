da31554c1ac68da57ad0863c9402bdf33e50645b
Holy-Person <holyochi@gmail.com>
2026-01-25 17:50:30 +0100

    Add Documentation Button

--- barkvr-system/ui/3dPanel/editmode/inspector/panels/panel_properties.gd
+++ barkvr-system/ui/3dPanel/editmode/inspector/panels/panel_properties.gd
@@ -5,9 +5,6 @@ extends InspectorPanel
 @onready var target_name_line: LineEdit = %TargetName
 
 @onready var button_documentation: Button = %ButtonDocumentation
-@onready var button_previous: Button = %ButtonPrevious
-@onready var button_next: Button = %ButtonNext
-@onready var button_history: Button = %ButtonHistory
 
 @onready var property_list: InspectorPropertyList = %PropertyList
 
@@ -15,17 +12,25 @@ extends InspectorPanel
 
 func _ready() -> void:
 	target_name_line.text_changed.connect(_on_target_name_line_text_changed)
+	button_documentation.pressed.connect(_on_button_documentation_pressed)
 
 func _on_target_set(new_target: Node) -> void:
-	if not is_instance_valid(new_target): return
+	var is_target_valid: bool = is_instance_valid(new_target)
+
+	target_name_line.editable = is_target_valid
+	button_documentation.disabled = not is_target_valid
+
+	property_list.edit.call_deferred(new_target)
+
+	if not is_target_valid:
+		target_name_line.text = ""
+		return
 
 	# Set text in TargetName LineEdit.
 	if new_target.has_meta(&"display_name"):
 		target_name_line.text = new_target.get_meta(&"display_name")
 	elif "name" in new_target and new_target.name:
 		target_name_line.text = new_target.name
-
-	property_list.edit.call_deferred(new_target)
 
 
 
@@ -35,3 +40,10 @@ func _on_target_name_line_text_changed(new_text: String) -> void:
 
 	target.set_meta(&"display_name", new_text)
 	target.name = target.name
+
+func _on_button_documentation_pressed() -> void:
+	if not is_instance_valid(target): return
+
+	var version_info: Dictionary = Engine.get_version_info()
+	var target_class: String = target.get_class().to_lower()
+	OS.shell_open("https://docs.godotengine.org/en/%s.%s/classes/class_%s.html" % [version_info.major, version_info.minor, target_class])
