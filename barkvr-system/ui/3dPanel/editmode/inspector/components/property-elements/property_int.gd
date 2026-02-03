5b13a8e855fd9c1f3e89f8f17e67d9d46755e8f3
Holy-Person <holyochi@gmail.com>
2026-02-03 14:43:58 +0100

    Int Property Functionality

--- barkvr-system/ui/3dPanel/editmode/inspector/components/property-elements/property_int.gd
+++ barkvr-system/ui/3dPanel/editmode/inspector/components/property-elements/property_int.gd
@@ -1,8 +1,12 @@
 class_name PropertyInt
 extends PropertyBase
+## Property editor for integer properties.
+##
+## Displays a spinbox to edit integers with.
 
 
 
+## The spinbox used to display and edit the property.
 @onready var spin_box: SpinBox = %SpinBox
 
 
@@ -10,29 +14,23 @@ extends PropertyBase
 func _setup() -> void:
 	spin_box.value_changed.connect(_on_spin_box_value_changed)
 
-func _update_visual() -> void:
-	if not is_instance_valid(target):
-		target = null
-		spin_box.editable = false
-		spin_box.set_value_no_signal(0)
-		return
+	# Prevent null revert values.
+	if property_revert_value == null:
+		property_revert_value = 0
 
-	if property_name in target:
-		if target[property_name] == null: target[property_name] = null
-		#spin_box.set_value_no_signal(target[property_name])
+func _update_visual() -> void:
+	# Do not set SpinBox value to null.
+	if checkable and target.get(property_name) == null:
+		spin_box.set_value_no_signal(property_revert_value)
+		return
+	spin_box.set_value_no_signal(target.get(property_name))
 
 
 
 func _on_spin_box_value_changed(value: float) -> void:
-	if not is_instance_valid(target) or not is_instance_valid(event_manager): return
-
-	event_manager.set_property(
-		event_manager.root.get_path_to(target),
-		property_name,
-		int(value)
-	)
+	set_value( int(value) )
 
 
 
 func _get_editing_state() -> bool:
-	return spin_box.get_line_edit().has_focus()
+	return spin_box.get_line_edit().is_editing()
